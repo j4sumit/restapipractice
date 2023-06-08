da507f1947f83d3d315b009548e9c225eba6e3d7
Sumit Kumar <j4sumit@gmail.com>
2023-06-08 23:25:40 +0530

    connection done with mongodb also check in postman

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express() ;
-
-const studentRoute = require("./api/routes/student")
-app.use("/student", studentRoute)
-
 const facultyRoute = require("./api/routes/faculty")
-app.use("/faculty", facultyRoute)
+const studentRoute = require("./api/routes/student")
+const mongoose = require("mongoose");
+mongoose.connect('mongodb+srv://jb4sumit:ToEJVVS1PzjoqkZB@sumit.pqdoicm.mongodb.net/');
+mongoose.connection.on("error", err =>{
+    console.log('connection failed');
+})
+mongoose.connection.on('connected', connected =>{
+    console.log("connected with database.....");
+})
 
+app.use("/student", studentRoute)
+app.use("/faculty", facultyRoute)
 
 app.use((req, res, next)=>{
     res.status(404).json({
