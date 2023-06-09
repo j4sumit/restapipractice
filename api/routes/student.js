074bf349d568c7580a4dc95fc7cdb0e93422334a
Sumit Kumar <j4sumit@gmail.com>
2023-06-09 14:24:38 +0530

    removed node_modules folder

--- api/routes/student.js
+++ api/routes/student.js
@@ -18,6 +18,25 @@ Student.find()
 })
 })
 
+//second get
+
+router.get("/:id",(req,res,next) =>{
+console.log(req.params.id);
+Student.findById(req.params.id)
+.then(result =>{
+    res.status(200).json({
+        student : result
+    })
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error: err
+    })
+})
+})
+
+
 router.post('/',(req, res, next)=>{
     const student= new Student({
         _id : new mongoose.Types.ObjectId,
